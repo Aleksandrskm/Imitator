d6db3ecac323cb72bcfec352fd3c2c6462dc8697
Aleksandrskm <aleksandr.skm@mail.ru>
2024-10-28 10:41:21 +0300

    стер лишнее

--- website/utils/utils.py
+++
@@ -1,6 +0,0 @@
-import math
-import requests
-from datetime import datetime, timedelta, timezone
-from skyfield.api import load
-from skyfield.sgp4lib import EarthSatellite
-from skyfield.toposlib import wgs84
