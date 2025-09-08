fefa31903c86a1793b5b4b22bca167437b616760
Вячеслав <62754486+vbelyavcev@users.noreply.github.com>
2025-09-08 16:54:17 +0300

    отображение страниц

--- website/views.py
+++ website/views.py
@@ -14,3 +14,9 @@ def function_call():
 def function_call_flow():
     return render_template('call_flow_simulator.html')
 
+@views.route('/calls_simulator')
+def function_calls():
+    return render_template('calls_simulator.html')
+@views.route('/call_flow_sov')
+def function_calls_flow():
+    return render_template('call_flow_sov.html')
